170dfcebcf52a9aa342f7922ac624ebeda0794ac
Yanderella! <yanderelladev@len.meek.moe>
2019-01-19 10:50:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('Now I am ready for some good, good, @SoMeOnE');
-    client.user.setActivity('@?someone for some good, good, SOMEONE');
+    client.user.setActivity('@someone for some good, good, SOMEONE');
 });
 
 client.on('message', msg => {
