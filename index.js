bd35019c4390ff73f6c2848bc62c5f08f226ab20
Yanderella! <yanderelladev@len.meek.moe>
2019-01-19 11:56:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,21 @@
 
 const { Client } = require('discord.js');
 const client = new Client();
+
 const config = require('./config.json');
+
+const { Client: dbiClient } = require('dbi.js');             
+const dbi = new dbiClient(config.dbiToken, { id: '536092396375769088' }); 
 
 client.on('ready', () => {
     console.log('Now I am ready for some good, good, @SoMeOnE');
     client.user.setActivity('@someone for some good, good, SOMEONE');
+
+    setInterval( () => {
+        dbi.postStats(client.guilds.size).then(console.log).catch(console.error);    // Posting your guild count to DBI and logging all responses
+    }, 60000)
 });
+
 
 client.on('message', msg => {
     if (msg.author.bot) return;
